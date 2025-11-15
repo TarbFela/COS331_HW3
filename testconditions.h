351db89cc51bd42135b1cb3a64e3aa842b117b8f
Maxim LaPlante <maxim.laplante@maine.edu>
2025-11-15 17:08:32 -0500

    Forgot the header ):

---
+++ testconditions.h
@@ -0,0 +1,16 @@
+//
+// Created by The Tragedy of Darth Wise on 11/7/25.
+//
+
+#ifndef HW3_TESTCONDITIONS_H
+#define HW3_TESTCONDITIONS_H
+
+typedef struct {
+    int np;
+    int nt;
+    //void (*fn)(void *arg);
+} testconditions_t;
+
+
+
+#endif //HW3_TESTCONDITIONS_H
